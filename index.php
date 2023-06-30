c9b99d60a82782353238152f56bea7a9d049506a
HytoSama <yanis.mekkid@icloud.com>
2023-06-30 16:28:39 +0200

    Modif Index.php
    Ajout de la gestion du nombre de place en fonction du nombre d'inscrit

--- index.php
+++ index.php
@@ -12,9 +12,16 @@ if ($conn->connect_error) {
     die("Échec de la connexion à la base de données: " . $conn->connect_error);
 }
 
-// Récupération des événements depuis la base de données
-$sql = "SELECT * FROM qr_event";
+// Récupération des événements depuis la base de données avec le nombre de places restantes
+$sql = "SELECT id, eventName, eventDesc, eventDate, eventPlace, COUNT(id) AS nbInscrits
+        FROM qr_event
+        LEFT JOIN qr_inscription_data i ON id = idEvenement
+        GROUP BY id";
 $result = $conn->query($sql);
+
+if (!$result) {
+    die("Erreur dans la requête SQL: " . $conn->error);
+}
 
 $evenements = array();
 if ($result->num_rows > 0) {
@@ -23,15 +30,20 @@ if ($result->num_rows > 0) {
         $nom = $row["eventName"];
         $description = $row["eventDesc"];
         $date = $row["eventDate"];
-        $place = $row["eventPlace"];
+        $placeTotal = $row["eventPlace"];
+        $nbInscrits = $row["nbInscrits"];
+        $placeRestantes = $placeTotal - $nbInscrits;
 
         $evenements[$id] = array(
             "nom" => $nom,
             "description" => $description,
             "date" => $date,
-            "place" => $place
+            "placeTotal" => $placeTotal,
+            "placeRestantes" => $placeRestantes
         );
     }
+} else {
+    echo "Aucun événement trouvé.";
 }
 
 // Vérifier si un événement a été sélectionné
@@ -47,25 +59,29 @@ if (isset($_POST['evenement'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Acceuil</title>
-    <link rel="stylesheet" href="css\style.css" type="text/css"/>
+    <title>Accueil</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
 </head>
 <body>
 
 <a class="admin-button" href="admin-login.php">Connexion administrateur</a>
 
-<h1>Sélectionnez un événement </h1>
+<h1>Sélectionnez un événement</h1>
 <div class="event-list">
     <?php foreach ($evenements as $id => $evenement) : ?>
         <div class="event-card">
             <h2><?php echo $evenement["nom"]; ?></h2>
             <p><?php echo $evenement["description"]; ?></p>
             <p>Date: <?php echo $evenement["date"]; ?></p>
-            <p>Nombre de Place: <?php echo $evenement["place"]; ?></p>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <input type="hidden" name="evenement" value="<?php echo $id; ?>">
-                <input type="submit" value="Inscription">
-            </form>
+            <?php if ($evenement["placeRestantes"] > 0) : ?>
+                <p>Nombre de places restantes: <?php echo $evenement["placeRestantes"]; ?></p>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <input type="hidden" name="evenement" value="<?php echo $id; ?>">
+                    <input type="submit" value="Inscription">
+                </form>
+            <?php else : ?>
+                <p>Toutes les places ont été occupées.</p>
+            <?php endif; ?>
         </div>
     <?php endforeach; ?>
 </div>
